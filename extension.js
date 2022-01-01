fe24ba9d3d57a25c9d6759a4aa62ed40befc0214
simanasch <tktossi@live.com>
2022-01-01 18:21:45 +0900

    grpc経由でvoiceroidを使用してttsする処理を追加

--- extension.js
+++ extension.js
@@ -1,7 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+const { execFile } = require('child_process');
 const vscode = require('vscode');
-
+const client = require("./src/client.js");
+// import { sayHello } from './src/client';
+let grpcServerProcess;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -9,12 +12,11 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "gyouyomi" is now active!');
-
-	// The command has been defined in the package.json file
+		
+		// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('gyouyomi.helloWorld', function () {
@@ -22,9 +24,29 @@ function activate(context) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from gyouyomi!');
+		
+		client.getLibraryList("ついなちゃん")
+		.then(results => {
+			// vscode.window.showInformationMessage(res);
+			console.info(results);
+		});
+		client.talk("ついなちゃんやで")
+		.then(res => {
+			vscode.window.showInformationMessage(res);
+			// console.info(results);
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	grpcServerProcess = execFile(
+		"C:/Users/tktos/Documents/repos/descript-to-video/TTSController/src/SpeechGRPCServer/bin/x64/Release/net4.5.2/SpeechGRpcServer.exe",
+		(error, stdout, stderr) => {
+			if (error) {
+				throw error;
+			}
+			console.log("stdout"+ stdout);
+			context.subscriptions.push(disposable);
+		}
+	);
 }
 
 // this method is called when your extension is deactivated
---
+++ src/client.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const PROTO_PATH = path.join(__dirname,'proto','TTSController.proto');
+console.info(PROTO_PATH);
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    }
+)
+const ttsProto = grpc.loadPackageDefinition(packageDefinition)
+
+const makeTTSService = (port) => {
+  return new ttsProto.ttscontroller.TTSService('localhost:'+port, grpc.credentials.createInsecure())
+}
+
+const talk = text => {
+  return new Promise((resolve, reject) => {
+    makeTTSService("5001")
+    .talk({
+      LibraryName: "ついな",
+      EngineName: "VOICEROID64",
+      Body: text,
+      OutputPath: "C://Users//tktos//Desktop//test.wav"
+    }, (error, response) => {
+      if (!error && response.IsSuccess) {
+        console.log(response.message) //こんにちわ ID:1太郎
+        resolve(text);
+      } else {
+        console.error(error);
+        reject(error);
+      }
+    })
+  })
+}
+
+const getLibraryList = text => {
+  return new Promise((resolve, reject) => {
+    makeTTSService("5001")
+    .getSpeechEngineDetail({
+      EngineName: "ついな",
+    }, (error, response) => {
+      if (!error ) {
+        // console.log(response.message) //こんにちわ ID:1太郎
+        resolve(response.detailItem);
+      } else {
+        console.error(error);
+        reject(error);
+      }
+    })
+  })
+}
+
+module.exports = {
+  getLibraryList,
+  talk
+}
