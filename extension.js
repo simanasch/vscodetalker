14f649c3d68f393180854d68da9f9937ef58d338
simanasch <tktossi@live.com>
2022-01-12 00:48:18 +0900

    読み上げに使用するttsエンジンを選択できるようにした

--- extension.js
+++ extension.js
@@ -8,6 +8,13 @@ const iconv = require('iconv-lite');
 let ttsControllerPath = path.join(__dirname,"bin","SpeechGRpcServer.exe");
 let grpcServerProcess;
 
+// エンジン名の変換定義
+const engineLabelTranslations = {
+	"VOICEROID64": "VOICEROID2"
+}
+const invertKeyValue = (o,r={})  => Object.keys(o).map(k => r[o[k]]=k) && r;
+const translateEngineName = (t,m) => !m[t] ? t : m[t];
+
 /**
  * 拡張機能の有効化時、一度だけ実行される関数
  * @param {vscode.ExtensionContext} context
@@ -50,7 +57,12 @@ function getLibraryList() {
 		console.info(results);
 		// settings.jsonに保存している使用可能なライブラリの一覧を更新する
 		// pcごとに1設定あればいいのでグローバル設定に保存
-		vscode.workspace.getConfiguration().update("gyouyomi.availableEngines", results, true)
+		vscode.workspace.getConfiguration().update("gyouyomi.availableEngines", results.map(t => {
+			return {
+				EngineName: t.EngineName,
+				LibraryName: t.LibraryName
+			}
+		}), true)
 	});
 }
 
@@ -59,16 +71,24 @@ function getLibraryList() {
  * @param {vscode.TextEditor} textEditor 
  * 現在のカーソル行を読み上げる
  */
-function talk(textEditor) {
+async function talk(textEditor) {
 	let ttsText = getTtsText(textEditor);
 	let config = vscode.workspace.getConfiguration("gyouyomi");
-	// デフォルトで使用するライブラリ名、利用可能なライブラリ名から読み上げに使用するライブラリの設定値を取得
-	let engine = getTtsEngine(config.get("defaultLibraryName"), config.get("availableEngines"));
 
-	client.talk(ttsText, engine.LibraryName, engine.EngineName)
-	.then(res => {
-		vscode.window.showInformationMessage("\""+ res + "\"を再生しました");
-	});
+	// quickPickで読み上げに使用するttsエンジンを選択する
+	const engine = await selectTtsEngine(config);
+	// 結果を表示する
+	if (engine) {
+		// 入力完了
+		client.talk(ttsText, engine.LibraryName, engine.EngineName)
+		.then(res => {
+			// showQuickPick()
+			vscode.window.showInformationMessage("\""+ res + "\"を再生しました");
+		});
+	} else {
+		// 入力がキャンセルされた
+		vscode.window.showInformationMessage(`再生をキャンセルしました`);
+	}
 }
 
 /**
@@ -76,23 +96,28 @@ function talk(textEditor) {
  * @param {vscode.TextEditor} textEditor 
  * 現在のカーソル行を読み上げ+録音する
  */
-function record(textEditor) {
+async function record(textEditor) {
 	let ttsText = getTtsText(textEditor);
 	let config = vscode.workspace.getConfiguration("gyouyomi");
-	// デフォルトで使用するライブラリ名、利用可能なライブラリ名から読み上げに使用するライブラリの設定値を取得
-	let engine = getTtsEngine(config.get("defaultLibraryName"), config.get("availableEngines"));
 	let currentFilePath = path.dirname(textEditor.document.fileName);
 	let pathConfig = config.get("defaultSavePath",path.join(currentFilePath,"tts"));
-	let saveToPath = generateRecordPath(pathConfig, engine.LibraryName, ttsText);
+	// quickPickで読み上げに使用するttsエンジンを選択する
+	const engine = await selectTtsEngine(config);
+	if (engine) {
+		let saveToPath = generateRecordPath(pathConfig, engine.LibraryName, ttsText);
 
-	client.record(ttsText, engine.LibraryName, engine.EngineName, saveToPath)
-	.then(res => {
-		if(config.get("saveTextFileOnRecord")) {
-			const buf = iconv.encode(engine.LibraryName+"＞"+ttsText, "Shift_JIS");
-			fs.writeFileSync(res.OutputPath.replace(/\.wav$/,".txt"),buf);
-		}
-		vscode.window.showInformationMessage("\"" + res.OutputPath + "\"を保存しました");
-	});
+		client.record(ttsText, engine.LibraryName, engine.EngineName, saveToPath)
+		.then(res => {
+			if(config.get("saveTextFileOnRecord")) {
+				const buf = iconv.encode(engine.LibraryName+"＞"+ttsText, "Shift_JIS");
+				fs.writeFileSync(res.OutputPath.replace(/\.wav$/,".txt"),buf);
+			}
+			vscode.window.showInformationMessage("\"" + res.OutputPath + "\"を保存しました");
+		});
+	} else {
+		// 入力がキャンセルされた
+		vscode.window.showInformationMessage(`録音をキャンセルしました`);
+	}
 }
 
 /**
@@ -107,9 +132,33 @@ const getTtsText = textEditor => {
 		: textEditor.document.getText(textEditor.selection)
 }
 
-const getTtsEngine = (libraryName, availableEngines) => {
-	let engines = availableEngines.filter(i => i.LibraryName === libraryName);
-	return engines.length > 0 ? engines[0] : availableEngines[0];
+/**
+ * @description 引数のワークスペース設定から音声合成エンジン選択のquickPickを作成する
+ * @param {vscode.WorkspaceConfiguration} config 
+ * @returns {Object}
+ */
+const selectTtsEngine = config => {
+	let defaultLibraryName = config.get("defaultLibraryName");
+	let quickPickItems = config.get("availableEngines").map(t => {
+		return {
+			"description": translateEngineName(t.EngineName, engineLabelTranslations),
+			"label": t.LibraryName
+		}
+	}).sort(t => t.label === defaultLibraryName ? -1 : 0)
+
+	// @ts-ignore
+	return vscode.window.showQuickPick(quickPickItems, {
+		placeHolder: "読み上げに使うライブラリ名"
+	})
+	.then(result => {
+		if(!result) return undefined;
+		return {
+			// @ts-ignore
+			"LibraryName": result.label, 
+			// @ts-ignore
+			"EngineName": translateEngineName(result.description, invertKeyValue(engineLabelTranslations))
+		}
+	})
 }
 
 const generateRecordPath = (dirName, ...args) => {
