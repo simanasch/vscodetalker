f1324069c3812c4a6aa61e6793b0dedf76273e5c
simana_sch <tktossi@live.com>
2022-02-01 23:45:31 +0900

    isEmptyに対するテストを追加、処理改良(falthyな値の場合isEmpty==trueとした)

--- src/util.js
+++ src/util.js
@@ -1,8 +1,8 @@
 // 引数がTruthyな値かの判別
 const isTruthy = t => !!t
 
-// Array系の型に対するisBlank判定
-const isEmpty = l => !Array.isArray(l) || l.length <= 0
+// 空Array判定
+const isEmpty = l => !isTruthy(l) || l.length <= 0
 
 // Objectのkey,valueを逆にする
 const invertKeyValue = (o,r={})  => Object.keys(o).map(k => r[o[k]]=k) && r;
--- test/suite/util.test.js
+++ test/suite/util.test.js
@@ -4,12 +4,20 @@ const util = require('../../src/util');
 
 suite('util関数に対するテスト', () => {
 
-	test('isTruthyのテスト', () => {
-		assert.deepStrictEqual(true, util.isTruthy(1));
-		assert.deepStrictEqual(true, util.isTruthy("hoge"));
-		assert.deepStrictEqual(false, util.isTruthy(""));
-		assert.deepStrictEqual(false, util.isTruthy(undefined));
-		assert.deepStrictEqual(false, util.isTruthy(null));
-	});
+  test('isTruthyのテスト', () => {
+    assert.deepStrictEqual(true, util.isTruthy(1));
+    assert.deepStrictEqual(true, util.isTruthy("hoge"));
+    assert.deepStrictEqual(false, util.isTruthy(""));
+    assert.deepStrictEqual(false, util.isTruthy(undefined));
+    assert.deepStrictEqual(false, util.isTruthy(null));
+  });
+
+  test('isEmptyのテスト',() => {
+    assert.deepStrictEqual(true, util.isEmpty([]));
+    assert.deepStrictEqual(false, util.isEmpty([null]));
+    assert.deepStrictEqual(false, util.isEmpty([undefined]));
+    assert.deepStrictEqual(true, util.isEmpty(undefined));
+    assert.deepStrictEqual(true, util.isEmpty(null));
+  })
 
 })
