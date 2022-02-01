726eb99dbe0cdeae733206a7391808edcc0b2a1b
simana_sch <tktossi@live.com>
2022-02-01 23:48:53 +0900

    util関数に対して型ヒント用のコメントを追加

--- src/util.js
+++ src/util.js
@@ -1,10 +1,23 @@
-// 引数がTruthyな値かの判別
+/**
+ * 引数がTruthyな値かの判別
+ * @param {any} t 
+ * @returns {Boolean}
+ */
 const isTruthy = t => !!t
 
-// 空Array判定
+/**
+ * 空Array判定
+ * @param {Array} l
+ * @returns {Boolean}
+ */
 const isEmpty = l => !isTruthy(l) || l.length <= 0
 
-// Objectのkey,valueを逆にする
+/**
+ * Objectのkey,valueを逆にする
+ * @param {Object} o
+ * @param {Object} r
+ * @returns {Boolean}
+ */
 const invertKeyValue = (o,r={})  => Object.keys(o).map(k => r[o[k]]=k) && r;
 
 module.exports = {
